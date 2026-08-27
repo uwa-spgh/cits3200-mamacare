679ba2e40fe0ab394e223aa5bb9f904d193f527d
Angelica Valencia <avalenciab.0108@gmail.com>
2026-08-27 10:21:02 +0800

    Dependencies instalation and main folder structure.

---
+++ src/components/views/AppSafeView.tsx
@@ -0,0 +1,39 @@
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from "react-native";
+import React, { FC } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AppColors } from "@/src/styles/colors";
+import { IS_IOS } from "@/src/constants/constants";
+
+interface AppSafeViewProps {
+  children: React.ReactNode;
+  style: ViewStyle;
+}
+
+const AppSafeView: FC<AppSafeViewProps> = ({ children, style }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, style]}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+export default AppSafeView;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: AppColors.background,
+    paddingTop: IS_IOS ? 0 : StatusBar.currentHeight || 0,
+  },
+
+  container: {
+    flex: 1,
+  },
+});
---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const AppColors = {
+    primary: '#AE214D',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#808080',
+    lightGray: '#D3D3D3',
+    darkGray: '#A9A9A9',
+    background: '',
+    disabled: '',
+}
